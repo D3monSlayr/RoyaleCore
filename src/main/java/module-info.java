472342e5da456e0e86111e805f616bcf3c5c7532
Kythera <dmonslayr286@gmail.com>
2025-11-25 13:57:54 +0300

    Did some build stuff. First push!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,13 @@
-module RoyaleCore.main {
+
+open module RoyaleCore.main {
     requires org.bukkit;
     requires static lombok;
-    requires brigadier;
-    requires RoyaleCore.main;
-    exports dev.royalcore.api;
-    exports dev.royalcore.api.registry;
+
+    exports dev.royalcore.api.item;
+    exports dev.royalcore.api.registrar;
+    exports dev.royalcore.api.br;
+    exports dev.royalcore.api.module;
+    exports dev.royalcore.api.scenario;
+    exports dev.royalcore.api.settings;
 
 }
